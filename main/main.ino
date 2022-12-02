6633bad831bcb07e9845631c373d4ae1552cd558
gbdngb12 <gbdngb12@naver.com>
2022-12-02 16:44:48 +0900

    thread

--- main/main.ino
+++ main/main.ino
@@ -8,14 +8,37 @@
 #define BT_TXD 7
 SoftwareSerial bluetooth(BT_RXD, BT_TXD);
 
+SoftwareSerial gpsSerial(11, 12);
 #include "MPU6050_6Axis_MotionApps20.h"
 
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
 #endif
+#include <TinyGPS.h>
 
 
 MPU6050 mpu;
+TinyGPS gps;
+
+// The getgps function will get and print the values we want.
+void getgps(TinyGPS &gps)
+{
+  // To get all of the data into varialbes that you can use in your code, 
+  // all you need to do is define variables and query the object for the 
+  // data. To see the complete list of functions see keywords.txt file in 
+  // the TinyGPS and NewSoftSerial libs.
+  
+  // Define the variables that will be used
+  // float latitude, longitude;
+  // Then call this function
+  float latitude, longitude;
+  gps.f_get_position(&latitude, &longitude);
+  // You can now print variables latitude and longitude
+  Serial.print("Lat/Long: "); 
+  Serial.print(latitude,5); 
+  Serial.print(", "); 
+  Serial.println(longitude,5);
+}
 
 #define OUTPUT_READABLE_YAWPITCHROLL
 
@@ -61,7 +84,7 @@ bool lock = false;
 bool flag = false;
 
 typedef struct location {
-  float x,y,z;
+  float x, y, z;
   float lat, lon;
   bool isMagnetic;
 
@@ -80,220 +103,271 @@ float zd = 10;
 // ===                      INITIAL SETUP                       ===
 // ================================================================
 
-void setup() {
-  // join I2C bus (I2Cdev library doesn't do this automatically)
+#include <mthread.h>
+
+// Our custom Thread:
+class FooThread : public Thread
+{
+  public:
+    FooThread(int id);
+  protected:
+    bool loop();
+  private:
+    int id;
+};
+
+FooThread::FooThread(int id)
+{
+  this->id = id;
+  if (this->id == 1) { //GPS
+    //Setup
+    gpsSerial.begin(9600);
+    gpsSerial.setTimeout(10);
+    gps = TinyGPS();
+
+  } else if (this->id == 2) { //MPU
+    //Setup
+    // join I2C bus (I2Cdev library doesn't do this automatically)
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-  Wire.begin();
-  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    Wire.begin();
+    Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
 #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-  Fastwire::setup(400, true);
+    Fastwire::setup(400, true);
 #endif
 
-  // initialize serial communication
-  // (115200 chosen because it is required for Teapot Demo output, but it's
-  // really up to you depending on your project)
-  bluetooth.begin(9600); // 블루투스 통신 초기화
-  Serial.begin(9600);
-  while (!Serial); // wait for Leonardo enumeration, others continue immediately
+    // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
-  // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
-  // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
-  // the baud timing being too misaligned with processor ticks. You must use
-  // 38400 or slower in these cases, or use some kind of external separate
-  // crystal solution for the UART timer.
+    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
+    // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
+    // the baud timing being too misaligned with processor ticks. You must use
+    // 38400 or slower in these cases, or use some kind of external separate
+    // crystal solution for the UART timer.
 
-  // initialize device
-  Serial.println(F("Initializing I2C devices..."));
-  mpu.initialize();
-  pinMode(INTERRUPT_PIN, INPUT);
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
 
-  // verify connection
-  Serial.println(F("Testing device connections..."));
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-  // wait for ready
-  Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-  while (Serial.available() && Serial.read()); // empty buffer
-  while (!Serial.available());                 // wait for data
-  while (Serial.available() && Serial.read()); // empty buffer again
+    // wait for ready
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
 
-  // load and configure the DMP
-  Serial.println(F("Initializing DMP..."));
-  devStatus = mpu.dmpInitialize();
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
 
-  // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXGyroOffset(220);
-  mpu.setYGyroOffset(76);
-  mpu.setZGyroOffset(-85);
-  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
 
-  // make sure it worked (returns 0 if so)
-  if (devStatus == 0) {
-    // Calibration Time: generate offsets and calibrate our MPU6050
-    mpu.CalibrateAccel(6);
-    mpu.CalibrateGyro(6);
-    mpu.PrintActiveOffsets();
-    // turn on the DMP, now that it's ready
-    Serial.println(F("Enabling DMP..."));
-    mpu.setDMPEnabled(true);
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+      // Calibration Time: generate offsets and calibrate our MPU6050
+      mpu.CalibrateAccel(6);
+      mpu.CalibrateGyro(6);
+      mpu.PrintActiveOffsets();
+      // turn on the DMP, now that it's ready
+      Serial.println(F("Enabling DMP..."));
+      mpu.setDMPEnabled(true);
 
-    // enable Arduino interrupt detection
-    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-    Serial.println(F(")..."));
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
+      // enable Arduino interrupt detection
+      Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+      Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+      Serial.println(F(")..."));
+      attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+      mpuIntStatus = mpu.getIntStatus();
 
-    // set our DMP Ready flag so the main loop() function knows it's okay to use it
-    Serial.println(F("DMP ready! Waiting for first interrupt..."));
-    dmpReady = true;
+      // set our DMP Ready flag so the main loop() function knows it's okay to use it
+      Serial.println(F("DMP ready! Waiting for first interrupt..."));
+      dmpReady = true;
 
-    // get expected DMP packet size for later comparison
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  } else {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
-    Serial.print(F("DMP Initialization failed (code "));
-    Serial.print(devStatus);
-    Serial.println(F(")"));
+      // get expected DMP packet size for later comparison
+      packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+      // ERROR!
+      // 1 = initial memory load failed
+      // 2 = DMP configuration updates failed
+      // (if it's going to break, usually the code will be 1)
+      Serial.print(F("DMP Initialization failed (code "));
+      Serial.print(devStatus);
+      Serial.println(F(")"));
+    }
+
+
+
   }
-
-  // configure LED for output
-  pinMode(LED_PIN, OUTPUT);
-
 }
 
-// ================================================================
-// ===                    MAIN PROGRAM LOOP                     ===
-// ================================================================
+bool FooThread::loop()
+{
+  if (this->id == 1) { //GPS
+    // Die if requested:
+    if (kill_flag)
+      return false;
 
+    String temp = "";
 
+    while (gpsSerial.available())    // While there is data on the RX
+    {
+      char c = gpsSerial.read();    // load the data into a variable...
 
-void loop() {
-  if (bluetooth.available()) {
-    int tmp = bluetooth.read();
-    if(tmp == '0') {
-       lock = false;
+      if (gps.encode(c))     // if there is a new valid sentence...
+      {
+        getgps(gps);         // then grab the data.
+      }
+
     }
-    else if(tmp == '1') {
-      lock = true;
-    }
-  }
-  // if programming failed, don't try to do anything
-  delay(1000);
-  if (!dmpReady) return;
-  // read a packet from FIFO
-  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet
+    return true;
+
+  } else if (this->id == 2) { //MPU
+    // Die if requested:
+    if (kill_flag)
+      return false;
+    // if programming failed, don't try to do anything
+    delay(1000);
+    if (!dmpReady) return;
+    // read a packet from FIFO
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet
 #ifdef OUTPUT_READABLE_QUATERNION
-    // display quaternion values in easy matrix form: w x y z
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    Serial.print("quat\t");
-    Serial.print(q.w);
-    Serial.print("\t");
-    Serial.print(q.x);
-    Serial.print("\t");
-    Serial.print(q.y);
-    Serial.print("\t");
-    Serial.println(q.z);
+      // display quaternion values in easy matrix form: w x y z
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      Serial.print("quat\t");
+      Serial.print(q.w);
+      Serial.print("\t");
+      Serial.print(q.x);
+      Serial.print("\t");
+      Serial.print(q.y);
+      Serial.print("\t");
+      Serial.println(q.z);
 #endif
 
 #ifdef OUTPUT_READABLE_EULER
-    // display Euler angles in degrees
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetEuler(euler, &q);
-    Serial.print("euler\t");
-    Serial.print(euler[0] * 180 / M_PI);
-    Serial.print("\t");
-    Serial.print(euler[1] * 180 / M_PI);
-    Serial.print("\t");
-    Serial.println(euler[2] * 180 / M_PI);
+      // display Euler angles in degrees
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetEuler(euler, &q);
+      Serial.print("euler\t");
+      Serial.print(euler[0] * 180 / M_PI);
+      Serial.print("\t");
+      Serial.print(euler[1] * 180 / M_PI);
+      Serial.print("\t");
+      Serial.println(euler[2] * 180 / M_PI);
 #endif
 
 #ifdef OUTPUT_READABLE_YAWPITCHROLL
-    // display Euler angles in degrees
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    float x = ypr[0] * 180 / M_PI;
-    float y = ypr[1] * 180 / M_PI;
-    float z = ypr[2] * 180 / M_PI;
-    if (lock == true && flag == false) {
-      //초기값 저장
-      loc.x = x;
-      loc.y = y;
-      loc.z = z;
-      //bluetooth.println("초기값! :x "+String(loc.x) + "y: "+String(loc.y) + "z: "+ String(loc.z));
-      //자석값 세팅 필요함
-      flag = true;
-    }
-    //값 비교
-    if(lock == true && flag == true) {
-      if(abs(loc.x - x) > xd) {
-        //bluetooth.println("x: " +String(x)+ "이상!");
+      // display Euler angles in degrees
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+      float x = ypr[0] * 180 / M_PI;
+      float y = ypr[1] * 180 / M_PI;
+      float z = ypr[2] * 180 / M_PI;
+      if (lock == true && flag == false) {
+        //초기값 저장
+        loc.x = x;
+        loc.y = y;
+        loc.z = z;
+        //bluetooth.println("초기값! :x "+String(loc.x) + "y: "+String(loc.y) + "z: "+ String(loc.z));
+        //자석값 세팅 필요함
+        flag = true;
       }
-      if(abs(loc.y - y) > yd) {
-        //bluetooth.println("y: "+ String(y) + "이상!");
+      //값 비교
+      if (lock == true && flag == true) {
+        if (abs(loc.x - x) > xd) {
+          //bluetooth.println("x: " +String(x)+ "이상!");
+        }
+        if (abs(loc.y - y) > yd) {
+          //bluetooth.println("y: "+ String(y) + "이상!");
+        }
+        if (abs(loc.z - z) > zd) {
+          //bluetooth.println("z: " + String(z) + "이상!");
+          bluetooth.println("35.133,129.104");
+        }
       }
-      if(abs(loc.z -z) > zd) {
-        //bluetooth.println("z: " + String(z) + "이상!");
-        bluetooth.println("35.133,129.104");
+      //잠금해제
+      if (lock == false) {
+        flag = false;
       }
-    }
-    //잠금해제
-    if(lock == false) {
-      flag = false;
-    }
 
 #endif
 
 #ifdef OUTPUT_READABLE_REALACCEL
-    // display real acceleration, adjusted to remove gravity
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetAccel(&aa, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-    Serial.print("areal\t");
-    Serial.print(aaReal.x);
-    Serial.print("\t");
-    Serial.print(aaReal.y);
-    Serial.print("\t");
-    Serial.println(aaReal.z);
+      // display real acceleration, adjusted to remove gravity
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      Serial.print("areal\t");
+      Serial.print(aaReal.x);
+      Serial.print("\t");
+      Serial.print(aaReal.y);
+      Serial.print("\t");
+      Serial.println(aaReal.z);
 #endif
 
 #ifdef OUTPUT_READABLE_WORLDACCEL
-    // display initial world-frame acceleration, adjusted to remove gravity
-    // and rotated based on known orientation from quaternion
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetAccel(&aa, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-    mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-    //Serial.print("aworld\t");
-    //Serial.print(aaWorld.x);
-    //Serial.print("\t");
-    //Serial.print(aaWorld.y);
-    //Serial.print("\t");
-    //Serial.println(aaWorld.z);
+      // display initial world-frame acceleration, adjusted to remove gravity
+      // and rotated based on known orientation from quaternion
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+      //Serial.print("aworld\t");
+      //Serial.print(aaWorld.x);
+      //Serial.print("\t");
+      //Serial.print(aaWorld.y);
+      //Serial.print("\t");
+      //Serial.println(aaWorld.z);
 #endif
 
 #ifdef OUTPUT_TEAPOT
-    // display quaternion values in InvenSense Teapot demo format:
-    teapotPacket[2] = fifoBuffer[0];
-    teapotPacket[3] = fifoBuffer[1];
-    teapotPacket[4] = fifoBuffer[4];
-    teapotPacket[5] = fifoBuffer[5];
-    teapotPacket[6] = fifoBuffer[8];
-    teapotPacket[7] = fifoBuffer[9];
-    teapotPacket[8] = fifoBuffer[12];
-    teapotPacket[9] = fifoBuffer[13];
-    Serial.write(teapotPacket, 14);
-    teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
+      // display quaternion values in InvenSense Teapot demo format:
+      teapotPacket[2] = fifoBuffer[0];
+      teapotPacket[3] = fifoBuffer[1];
+      teapotPacket[4] = fifoBuffer[4];
+      teapotPacket[5] = fifoBuffer[5];
+      teapotPacket[6] = fifoBuffer[8];
+      teapotPacket[7] = fifoBuffer[9];
+      teapotPacket[8] = fifoBuffer[12];
+      teapotPacket[9] = fifoBuffer[13];
+      Serial.write(teapotPacket, 14);
+      teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
 #endif
 
-    // blink LED to indicate activity
-    blinkState = !blinkState;
-    digitalWrite(LED_PIN, blinkState);
+
+
+      return true;
+
+    }
+
   }
 }
+
+void setup()
+{
+
+  // Create five threads and add them to the main ThreadList:
+  //for(int i = 1; i <= 5; i++)
+  //    main_thread_list->add_thread(new FooThread(i));
+  bluetooth.begin(9600); // 블루투스 통신 초기화
+  Serial.begin(9600);
+  main_thread_list->add_thread(new FooThread(1));//GPS
+  main_thread_list->add_thread(new FooThread(2));//MPU 6050
+  // Initialize the serial connection:
+  //Serial.begin(9600);
+  //delay(1000);
+
+}
+
+// jl
